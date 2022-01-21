f936dd06b07b038557e2f55fe2395da3afbfa5fe
Ronin Carrion <rdcarrion3@utpl.edu.ec>
2022-01-21 12:08:14 -0500

    ClasesSemana6

--- EjemploTres/src/ejemplotres/EjemploTres.java
+++ EjemploTres/src/ejemplotres/EjemploTres.java
@@ -17,18 +17,30 @@ public class EjemploTres {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // 
+
         int a = 20;
         int b = 10;
-        System.out.println(NumberUtils.compare(a, b));
-        System.out.println( obtenerDecimal("10.2") + 20 );
-        System.out.printf("El número mayor es: %.2f\n", 
-                NumberUtils.max(10.2, 1.1, 4.2) );
+        int c = NumberUtils.compare(a, b);
+        if (c == 1) {
+            System.out.printf("El número %s es mayor al número %s\n", a, b);
+        } else {
+            if (c == 0) {
+                System.out.printf("El número %s es igual al número %s\n", a, b);
+            } else {
+                if (c == -1) {
+                    System.out.printf("El número %s es menor al número %s\n", a, b);
+                }
+            }
+        }
+        // System.out.println(NumberUtils.compare(a, b));
+        // System.out.println( obtenerDecimal("10.2") + 20 );
+        // System.out.printf("El número mayor es: %.2f\n", 
+        //        NumberUtils.max(10.2, 1.1, 4.2) ;
     }
-    
-    public static double obtenerDecimal(String cadena){
+
+    public static double obtenerDecimal(String cadena) {
         double d = NumberUtils.createDouble(cadena);
         return d;
     }
-    
+
 }
